753b288da8aaec434bb0481b9f877cf46e8d5414
joyce102802 <moralesjoyceb@gmail.com>
2023-09-19 00:00:57 +0800

    main

--- app/Controllers/ProductController.php
+++ app/Controllers/ProductController.php
@@ -3,16 +3,23 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
+//use App\Models\ProductModel;
 
 class ProductController extends BaseController
 {
+    private $product;
+    public function _construct()
+    {
+        $this->product = new \App\Models\ProductModel();
+    }
     public function product($product)
     {
         echo $product;
     }
     public function tryy()
     {
-        return view('product');
+       $data = $this->product->findAll();
+       print_r($data);
     }
     public function index()
     {
